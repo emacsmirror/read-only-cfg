09789d6d41eb8092512de86bed7b30a993aaf5df
pfchen <pfchen31@gmail.com>
2021-05-09 19:00:11 +0800

    Rename to read-only-cfg

--- read-only-cfg.el
+++ read-only-cfg.el
@@ -1,4 +1,4 @@
-;;; read-only-cf.el --- Make files read-only based on user conf -*- lexical-binding: t; -*-
+;;; read-only-cfg.el --- Make files read-only based on user config -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021 pfchen
 
@@ -6,7 +6,7 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; Keywords: tools, convenience
-;; URL: https://github.com/pfchen/read-only-cf
+;; URL: https://github.com/pfchen/read-only-cfg
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 ;;
-;; `read-only-cf' is a GNU Emacs minor mode which can automatically make
+;; `read-only-cfg' is a GNU Emacs minor mode which can automatically make
 ;;  files read-only based on user configuration.  User configuration is
 ;;  very simple and it consists of prefix directories or regex patterns.
 
@@ -38,179 +38,179 @@
 ;;     (package-initialize)
 ;;     (package-refresh-contents)
 ;;
-;; Now you can install `read-only-cf' with:
+;; Now you can install `read-only-cfg' with:
 ;;
-;;     M-x package-install RET read-only-cf RET
+;;     M-x package-install RET read-only-cfg RET
 ;;
 ;; And enable with:
 ;;
-;;     (require 'read-only-cf)
-;;     (read-only-cf-mode 1)
+;;     (require 'read-only-cfg)
+;;     (read-only-cfg-mode 1)
 ;;
 ;;
 ;; Alternatively, you can manually download or clone this repository
 ;; locally, and add this to your init.el:
 ;;
-;;     (add-to-list 'load-path "/path/to/read-only-cf")
-;;     (autoload 'read-only-cf "read-only-cf" nil t)
-;;     (require 'read-only-cf)
-;;     (read-only-cf-mode 1)
+;;     (add-to-list 'load-path "/path/to/read-only-cfg")
+;;     (autoload 'read-only-cfg "read-only-cfg" nil t)
+;;     (require 'read-only-cfg)
+;;     (read-only-cfg-mode 1)
 
 ;; Usage
 ;;
 ;; Add a read-only directory:
 ;;
-;;     M-x read-only-cf-add-dir RET /path/to/you-directory RET
+;;     M-x read-only-cfg-add-dir RET /path/to/you-directory RET
 ;;
 ;; Add a read-only regex pattern:
 ;;
-;;     M-x read-only-cf-add-regexp RET <regexp> RET
+;;     M-x read-only-cfg-add-regexp RET <regexp> RET
 ;;
 ;; Or add this into your config:
 ;;
-;;     (require 'read-only-cf)
-;;     (read-only-cf-add-dir "/path/to/your-directory/")
-;;     (read-only-cf-add-regexp "<regexp>")
+;;     (require 'read-only-cfg)
+;;     (read-only-cfg-add-dir "/path/to/your-directory/")
+;;     (read-only-cfg-add-regexp "<regexp>")
 ;;
 ;; And remove a read-only directory:
 ;;
-;;     M-x read-only-cf-remove-dir RET /path/to/your-directory RET
+;;     M-x read-only-cfg-remove-dir RET /path/to/your-directory RET
 ;;
 ;; Remove a read-only regex pattern:
 ;;
-;;     M-x read-only-cf-remove-regexp RET <regexp> RET
+;;     M-x read-only-cfg-remove-regexp RET <regexp> RET
 ;;
 ;;
 ;; Customization
 ;;
-;; Customize variable `read-only-cf-update-file-buffer-state' to
+;; Customize variable `read-only-cfg-update-file-buffer-state' to
 ;; determine whether to update the read-only state of all existing
 ;; file-buffer when this mode is enabled or disabled.
 
 ;;; Code:
-(defgroup read-only-cf nil
+(defgroup read-only-cfg nil
   "Make files read-only based on config."
-  :prefix "read-only-cf-"
+  :prefix "read-only-cfg-"
   :group 'files)
 
-(defcustom read-only-cf-dirs
+(defcustom read-only-cfg-dirs
   ()
   "List of all user defined read-only directories."
   :type '(repeat (string :tag "Read-only directory"))
-  :group 'read-only-cf)
+  :group 'read-only-cfg)
 
-(defcustom read-only-cf-regexps
+(defcustom read-only-cfg-regexps
   ()
   "List of all user defined read-only regex patterns."
   :type '(repeat (regexp :tag "Read-only regex pattern"))
-  :group 'read-only-cf)
+  :group 'read-only-cfg)
 
-(defcustom read-only-cf-update-file-buffer-state
+(defcustom read-only-cfg-update-file-buffer-state
   t
   "Whether update buffer state when this mode is enabled or disabled.
 If this flag is non-nil, the read-only state of all existing
 buffers associated with files will be changed when the
-read-only-cf mode is enabled or disabled.
+read-only-cfg mode is enabled or disabled.
 
 Default is non-nil."
   :type '(boolean)
-  :group 'read-only-cf)
+  :group 'read-only-cfg)
 
-(defun read-only-cf--regexp-valid-p (string)
+(defun read-only-cfg--regexp-valid-p (string)
   "Return non-nil if STRING is a valid regex pattern."
   (condition-case err
       (prog1 t (string-match-p string ""))
     (error (message (error-message-string err)) nil)))
 
-(defun read-only-cf--match-dirs-p (file-name)
+(defun read-only-cfg--match-dirs-p (file-name)
   "Return non-nil if FILE-NAME matched user defined prefix directories."
   (catch 'done
-    (dolist (dir read-only-cf-dirs)
+    (dolist (dir read-only-cfg-dirs)
       (when (string-prefix-p dir (file-name-as-directory file-name))
         (throw 'done t)))
     nil))
 
-(defun read-only-cf--match-regexps-p (file-name)
+(defun read-only-cfg--match-regexps-p (file-name)
   "Return non-nil if FILE-NAME matched user defined regex patterns."
   (catch 'done
-    (dolist (regexp read-only-cf-regexps)
+    (dolist (regexp read-only-cfg-regexps)
       (when (string-match-p regexp file-name)
         (throw 'done t)))
     nil))
 
-(defun read-only-cf--match-p (file-name)
+(defun read-only-cfg--match-p (file-name)
   "Return non-nil if FILE-NAME matched user defined config."
-  (if (or (read-only-cf--match-dirs-p file-name)
-          (read-only-cf--match-regexps-p file-name))
+  (if (or (read-only-cfg--match-dirs-p file-name)
+          (read-only-cfg--match-regexps-p file-name))
       t
     nil))
 
-(defun read-only-cf--find-file-handler ()
+(defun read-only-cfg--find-file-handler ()
   "A read-only handler of `find-file-hook'."
-  (when (read-only-cf--match-p (buffer-file-name))
+  (when (read-only-cfg--match-p (buffer-file-name))
     (read-only-mode 1)))
 
-(defun read-only-cf--update-buffers (v)
+(defun read-only-cfg--update-buffers (v)
   "Update the read-only state of existing file buffers with value V."
-  (when read-only-cf-update-file-buffer-state
+  (when read-only-cfg-update-file-buffer-state
     (dolist (buf (buffer-list))
       (when (and (buffer-file-name buf)
-                 (read-only-cf--match-p (buffer-file-name buf)))
+                 (read-only-cfg--match-p (buffer-file-name buf)))
         (with-current-buffer buf
           (read-only-mode v))))))
 
 ;;;###autoload
-(defun read-only-cf-add-dir (dir)
+(defun read-only-cfg-add-dir (dir)
   "Add a read-only directory DIR."
   (interactive "DRead-only directory: ")
   (let ((dir (file-name-as-directory (file-truename dir))))
-    (add-to-list 'read-only-cf-dirs dir)
+    (add-to-list 'read-only-cfg-dirs dir)
     (message "Added a read-only directory: %s" dir)))
 
 ;;;###autoload
-(defun read-only-cf-add-regexp (regexp)
+(defun read-only-cfg-add-regexp (regexp)
   "Add a read-only regex pattern REGEXP."
   (interactive "sRead-only regex pattern: ")
-  (when (read-only-cf--regexp-valid-p regexp)
-    (add-to-list 'read-only-cf-regexps regexp)
+  (when (read-only-cfg--regexp-valid-p regexp)
+    (add-to-list 'read-only-cfg-regexps regexp)
     (message "Added a regex pattern: %s" regexp)))
 
 ;;;###autoload
-(defun read-only-cf-remove-dir (dir)
+(defun read-only-cfg-remove-dir (dir)
   "Remove a read-only directory DIR."
   (interactive "DRead-only directory: ")
   (let ((dir (file-name-as-directory (file-truename dir))))
-    (if (member dir read-only-cf-dirs)
+    (if (member dir read-only-cfg-dirs)
 	(progn
-	  (setq read-only-cf-dirs (delete dir read-only-cf-dirs))
+	  (setq read-only-cfg-dirs (delete dir read-only-cfg-dirs))
 	  (message "Removed a read-only directory: %s" dir))
       (error "%S is not in read-only directory list" dir))))
 
 ;;;###autoload
-(defun read-only-cf-remove-regexp (regexp)
+(defun read-only-cfg-remove-regexp (regexp)
   "Remove a read-only regex pattern REGEXP."
   (interactive "sRead-only regex pattern: ")
-  (if (and (read-only-cf--regexp-valid-p regexp)
-           (member regexp read-only-cf-regexps))
+  (if (and (read-only-cfg--regexp-valid-p regexp)
+           (member regexp read-only-cfg-regexps))
       (progn
-	(setq read-only-cf-regexps (delete regexp read-only-cf-regexps))
+	(setq read-only-cfg-regexps (delete regexp read-only-cfg-regexps))
 	(message "Removed a read-only regex pattern: %s" regexp))
     (error "%S is not in read-only regex pattern list" regexp)))
 
 ;;;###autoload
-(define-minor-mode read-only-cf-mode
+(define-minor-mode read-only-cfg-mode
   "Minor mode for making files read-only based on config."
   nil " RoCf" nil
   :global t
-  (if read-only-cf-mode
+  (if read-only-cfg-mode
       (progn
-        (add-hook 'find-file-hook #'read-only-cf--find-file-handler)
-        (read-only-cf--update-buffers 1))
+        (add-hook 'find-file-hook #'read-only-cfg--find-file-handler)
+        (read-only-cfg--update-buffers 1))
     (progn
-      (remove-hook 'find-file-hook #'read-only-cf--find-file-handler)
-      (read-only-cf--update-buffers -1))))
+      (remove-hook 'find-file-hook #'read-only-cfg--find-file-handler)
+      (read-only-cfg--update-buffers -1))))
 
-(provide 'read-only-cf)
+(provide 'read-only-cfg)
 
 ;; Local Variables:
 ;; coding: utf-8
@@ -218,4 +218,4 @@ Default is non-nil."
 ;; checkdoc-minor-mode: t
 ;; End:
 
-;;; read-only-cf.el ends here
+;;; read-only-cfg.el ends here
